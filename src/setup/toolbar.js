28bfab6b6cd40872ee35a981d343ec1da59bac8a
Steven G. Harris <steven.g.harris@gmail.com>
2025-06-04 09:32:43 -0700

    Fix the toolbar and spacer height

--- src/setup/toolbar.js
+++ src/setup/toolbar.js
@@ -78,6 +78,7 @@ class ToolbarView {
   }
 
   update() {
+    console.log("UPDATE Toolbar")
     if (this.editorView.root != this.root) {
       let { dom, update } = renderGrouped(this.editorView, this.content);
       this.contentUpdate = update;
@@ -87,14 +88,14 @@ class ToolbarView {
     this.contentUpdate(this.editorView.state);
     if (this.floating) {
       this.updateScrollCursor();
-    }
-    else {
+    } else {
       if (this.menu.offsetWidth != this.widthForMaxHeight) {
         this.widthForMaxHeight = this.menu.offsetWidth;
         this.maxHeight = 0;
       }
       if (this.menu.offsetHeight > this.maxHeight) {
-        this.maxHeight = this.menu.offsetHeight;
+        // Don't reset maxHeight because intermediate updates render as text, expanding height
+        // this.maxHeight = this.menu.offsetHeight;
         this.menu.style.minHeight = this.maxHeight + "px";
       }
     }
@@ -137,12 +138,13 @@ class ToolbarView {
       if (editorRect.top < top && editorRect.bottom >= this.menu.offsetHeight + 10) {
         this.floating = true;
         let menuRect = this.menu.getBoundingClientRect();
+        let spacerHeight = this.menu.firstChild.getBoundingClientRect().height + 10;
         this.menu.style.left = menuRect.left + "px";
         this.menu.style.width = menuRect.width + "px";
         if (scrollAncestor)
           this.menu.style.top = top + "px";
         this.menu.style.position = "fixed";
-        this.spacer = crel("div", { class: prefix + "-spacer", style: `height: ${menuRect.height}px` });
+        this.spacer = crel("div", { class: prefix + "-spacer", style: `height: ${spacerHeight}px` });
         parent.insertBefore(this.spacer, this.menu);
       }
     }
