4340ad14038f46c1c4819f0726c4eb030a84307f
Steven G. Harris <steven.g.harris@gmail.com>
2025-07-24 09:30:36 -0700

    Initial iteration on GitHub Pages site

---
+++ docs/src/messagehandler-browser.js
@@ -0,0 +1,26 @@
+/**
+ * The instance that will receive `postMessage` from the MarkupEditor as the document state changes.
+ */
+class MessageHandler {
+
+    /**
+     * Take action when messages we care about come in.
+     * @param {string | JSON} message   The message passed from the MarkupEditor as the state changes. 
+     */
+    postMessage(message) {
+        if (message.startsWith('input')) {
+            // Some input or change happened in the document, so update the raw HTML in the `htmldiv`.
+            // Generally, it will be too heavyweight for a real app to pull back the full HTML contents
+            // at every keystroke (or do much of anything), but it works nicely in the demo to show changes.
+            fileToolbar.updateRaw()
+            return;
+        }
+        switch (message) {
+            // The editor posts `ready` when all scripts are loaded, so we can set the HTML.
+            case 'ready': {
+                fileToolbar.hideRaw()
+                MU.setHTML(markupEditor.html, true)
+            }
+        }
+    }
+}
