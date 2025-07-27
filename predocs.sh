1fd2f68bebdfe553c07741dd776ee52e23aa1b95
Steven G. Harris <steven.g.harris@gmail.com>
2025-07-27 10:58:59 -0700

    Add docs and predocs scripts

---
+++ predocs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Updating ./docs dependencies..."
+set -v
+cp -f ./dist/markupeditor.umd.js ./docs/src/markupeditor.umd.js
+cp -f ./src/filetoolbar.js ./docs/src/filetoolbar.js
+cp -f ./styles/markupeditor.css ./docs/styles/markupeditor.css
+cp -f ./styles/markup.css ./docs/styles/markup.css
+cp -f ./styles/mirror.css ./docs/styles/mirror.css
+cp -f ./styles/toolbar.css ./docs/styles/toolbar.css
