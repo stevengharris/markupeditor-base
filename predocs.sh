9f89afda82a95ecd9f2691c797a843b68b378733
Steven G. Harris <steven.g.harris@gmail.com>
2025-12-06 10:41:36 -0800

    Move web component to project root dir

--- predocs.sh
+++ predocs.sh
@@ -3,7 +3,7 @@
 echo "Updating ./docs dependencies..."
 set -v
 cp -f ./dist/markupeditor.umd.js ./docs/src/markupeditor.umd.js
-cp -f ./webcomponent/markup-editor.js ./docs/src/markup-editor.js
+cp -f ./markup-editor.js ./docs/src/markup-editor.js
 cp -f ./styles/markupeditor.css ./docs/styles/markupeditor.css
 cp -f ./styles/markup.css ./docs/styles/markup.css
 cp -f ./styles/mirror.css ./docs/styles/mirror.css
