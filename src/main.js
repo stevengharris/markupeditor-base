fe9f450c702455dbbb5038639eb617ffbf68aa6e
Steven G. Harris <steven.g.harris@gmail.com>
2025-12-21 10:25:10 -0800

    Move class MarkupEditor to separate file

--- src/main.js
+++ src/main.js
@@ -1,398 +1,178 @@
-import {EditorState} from "prosemirror-state"
-import {EditorView} from "prosemirror-view"
-import {DOMParser} from "prosemirror-model"
-import {schema} from "./schema/index.js"
-import {markupSetup} from "./setup/index.js"
-
-import {
-  setTopLevelAttributes,
-  setMessageHandler,
-  loadUserFiles,
-  searchFor,
-  deactivateSearch,
-  cancelSearch,
-  pasteText,
-  pasteHTML,
-  emptyDocument,
-  getHTML,
-  getTestHTML,
-  setHTML,
-  setTestHTML,
-  getHeight,
-  padBottom,
-  focus,
-  focusOn,
-  resetSelection,
-  addDiv,
-  removeDiv,
-  addButton,
-  removeButton,
-  removeAllDivs,
-  toggleBold,
-  toggleItalic,
-  toggleUnderline,
-  toggleCode,
-  toggleStrike,
-  toggleSubscript,
-  toggleSuperscript,
-  setStyle,
-  replaceStyle,
-  toggleListItem,
-  indent,
-  outdent,
-  startModalInput,
-  endModalInput,
-  getSelectionState,
-  selectionChanged,
-  callbackInput,
-  clicked,
-  consoleLog,
-  doUndo,
-  doRedo,
-  resetSelectedID,
-  outermostOfTypeAt,
-  testBlockquoteEnter,
-  testListEnter,
-  testExtractContents,
-  testPasteHTMLPreprocessing,
-  testPasteTextPreprocessing,
-  insertLink,
-  deleteLink,
-  getDataImages,
-  savedDataImage,
-  insertImage,
-  modifyImage,
-  cutImage,
-  insertTable,
-  addRow,
-  addCol,
-  addHeader,
-  deleteTableArea,
-  borderTable,
-  handleEnter,
-  focused,
-  blurred,
-} from "./markup.js"
-
-import {LinkView} from "./nodeview/linkview.js"
-import {ImageView} from "./nodeview/imageview.js"
-import {DivView} from "./nodeview/divview.js"
-
-import {Searcher} from "./searcher.js"
-
-import {
-  registerEditor,
-  unregisterEditor,
-  activeEditor,
-  registerDelegate,
-  getDelegate,
-  registerConfig,
-  activeConfig,
-  getConfig,
-  registerMessageHandler,
-  getMessageHandler,
-  registerAugmentation,
-  getAugmentation,
-  setActiveView,
-} from "./registry.js"
-
-import { 
-  MenuItem,
-  Dropdown, 
-  DropdownSubmenu, 
-  cmdItem,
-  renderGrouped,
-  renderDropdownItems
- } from "./setup/menuitems.js"
-
-import {ToolbarConfig} from "./config/toolbarconfig.js"
-import {KeymapConfig} from "./config/keymapconfig.js"
-import {BehaviorConfig} from "./config/behaviorconfig.js"
-
-import { 
-  prependToolbar, 
-  appendToolbar,
-  toggleSearch,
-  openLinkDialog,
-  openImageDialog
-} from "./setup/index.js"
-
-import {MessageHandler} from "./messagehandler.js"
-import {toolbarView} from "./setup/toolbar.js"
+import mirrorStyle from "../styles/mirror.css" with { type: "css" }
+import markupStyle from "../styles/markup.css" with { type: "css" }
+import toolbarStyle from "../styles/toolbar.css" with { type: "css" }
+import { MU } from "./markupeditor.js"
+export { MU }
 
 /**
- * The MarkupEditor holds the properly set-up EditorView and any additional configuration.
+ * MarkupEditorElement is the Web Component for the MarkupEditor.
  * 
- * @param {HTMLElement} target  The div that will contain the editor.
- * @param {Object}      config  The configuration object. See the Developer's Guide.
+ * The lifecycle and resulting document structure are probably the most interesting 
+ * aspects of the MarkupEditorElement, especially because the HTML page can   
+ * contain multiple of them. The MarkupEditor "base" script should be loaded 
+ * only once in the first (or only) MarkupEditorElement. It defines the global
+ * `MU` along with the global `muRegistry` with exported methods to access it.
+ * 
+ * We use the `connectedCallback`, which is called for each MarkupEditorElement, 
+ * to trigger appending the MarkupEditor base script only once. It's loaded into 
+ * the first MarkupEditorElement, and produces the global `MU` that provides access
+ * to all editor functionality regardless of where subsequent scripts are run.
+ * When the base script finishes loading, we dispatch the `ready` `muCallback` 
+ * event for each MarkupEditorElement instance in `document`. From that point, 
+ * the MarkupEditor styling is appended to the `editor` set up for each individual 
+ * MarkupEditorElement instance. Any user-supplied script and styling are also 
+ * appended. Once those are appended (and even if they are not specified), the 
+ * `loadedUserFiles` `muCallback` is dispatched for the `editorContainer`, and 
+ * we can finally `createEditor` for the element and set its HTML contents.
  */
-class MarkupEditor {
-  constructor(target, config) {
+class MarkupEditorElement extends HTMLElement {
 
-    // We will be creating an EditorView in the `target` element, which should be a DIV
-    // with `id` set to "editor".
-    this.element = target ?? document.querySelector("#editor")
+  /** 
+   * Construct the MarkupEditorElement and set up the events to listen-to that 
+   * drive loading of scripts, styles, configuration, and contents.
+   */
+  constructor() {
+    super()   // Establish prototype chain
 
-    // Make sure config always contains `toolbar`, `keymap`, and `behavior`.
-    // The three configurations can be specified by string name that is 
-    // dereferenced to an instance using `getConfig`, or as an instance
-    // modeled on ToolbarConfig, KeymapConfig, or BehaviorConfig.
-    this.config = config ?? {}
+    // Attach shadow tree and hold onto root reference
+    // https://developer.mozilla.org/en-US/docs/Web/API/Element/attachShadow
+    const shadow = this.attachShadow({ mode: 'open' })
 
-    // Toolbar configuration
-    let toolbarConfig = this.config.toolbar
-    if (toolbarConfig) {
-      if (typeof toolbarConfig === 'string') {
-        // If the toolbarConfig is something that can't be found (e.g., 'none'),
-        // then set toolbar.visibility to false, which avoids creating a 
-        // toolbar at all.
-        this.config.toolbar = getConfig(toolbarConfig) ?? ToolbarConfig.none()
-      } else {
-        this.config.toolbar = toolbarConfig
-      }
-    } else {
-      this.config.toolbar = ToolbarConfig.standard()
-    }
+    // Create a container for the markup-editor component
+    this.editorContainer = document.createElement('div')
+    // Add a class to the container for the sake of clarity
+    this.editorContainer.classList.add('markup-editor')
+    this.editorContainer.setAttribute('id', 'editor')
 
-    // Keymap configuration
-    let keymapConfig = this.config.keymap
-    if (keymapConfig) {
-      if (typeof keymapConfig === 'string') {
-        this.config.keymap = getConfig(keymapConfig) ?? KeymapConfig.standard()
-      } else {
-        this.config.keymap = keymapConfig
-      }
-    } else {
-      this.config.keymap = KeymapConfig.standard()
-    }
-
-    // Behavior configuration
-    let behaviorConfig = this.config.behavior
-    if (behaviorConfig) {
-      if (typeof behaviorConfig === 'string') {
-        this.config.behavior = getConfig(behaviorConfig) ?? BehaviorConfig.standard()
-      } else {
-        this.config.behavior = behaviorConfig
-      }
-    } else {
-      this.config.behavior = BehaviorConfig.standard()
-    }
-
-    // If `delegate` is supplied as a string, then dereference it to get the class from muRegistry.
-    let delegate = this.config.delegate
-    if (delegate && (typeof delegate === 'string')) {
-      this.config.delegate = getDelegate(delegate)
-    }
-
-    // Create the EditorView for this MarkupEditor
-    this.view = new EditorView(this.element, {
-      state: EditorState.create({
-        // For the MarkupEditor, we can just use the editor element. 
-        // There is no need to use a separate content element.
-        doc: DOMParser.fromSchema(schema).parse(this.element),
-        plugins: markupSetup(this.config, schema)
-      }),
-      nodeViews: {
-        link(node, view, getPos) { return new LinkView(node, view, getPos)},
-        image(node, view, getPos) { return new ImageView(node, view, getPos) },
-        div(node, view, getPos) { return new DivView(node, view, getPos) },
-      },
-      // All text input makes callbacks to indicate the document state has changed.
-      // For history, used handleTextInput, but that fires *before* input happens.
-      // Note the `setTimeout` hack is used to have the function called after the change
-      // for things things other than the `input` event.
-      handleDOMEvents: {
-        'input': () => { callbackInput(target) },
-        'focus': () => { setTimeout(() => focused(this.element))},
-        'blur': () => { setTimeout(() => blurred(this.element))},
-        'cut': () => { setTimeout(() => { callbackInput(this.element) }, 0) },
-        'click': (view) => { setTimeout(() => { clicked(view, this.element) }, 0) },
-        'delete': () => { setTimeout(() => { callbackInput(this.element) }, 0) },
-      },
-      handlePaste() {
-        setTimeout(() => { callbackInput(this.element) }, 0)
-        return false
-      },
-      handleKeyDown(view, event) {
-        switch (event.key) {
-          case 'Enter':
-          case 'Delete':
-          case 'Backspace':
-            { setTimeout(() => { handleEnter() }, 0) }
-        }
-        return false
-      },
-      // Use createSelectionBetween to handle selection and click both.
-      // We need access to `this.editor` for `selectionChanged`.
-      // We use it guard against selecting across divs.
-      createSelectionBetween: this.createSelectionBetween.bind(this)
+    // Have the `editorContainer` listen for callbacks from within the 
+    // MarkupEditor base script, dispatched from `_callback`. The
+    // `messageHandler` can be overridden (as it is for VSCode and 
+    // Swift) so all document editing notifications can be dealt 
+    // with in a custom way.
+    // 
+    // The first `muCallback` will be `loadedUserFiles`, which will 
+    // cause the editor instance to be created before posting the 
+    // message.
+    this.editorContainer.addEventListener('muCallback', (e) => {
+      if (!this.editor) this.createEditor()
+      this.editor.messageHandler.postMessage(e.message)
     })
 
-    // The `messageHandler` is specific to this `editor` and is accessible from 
-    // `activeMessageHandler()` or directly from an editor instance. It can 
-    // be passed-in as a string that is dereferenced from the registry using 
-    // `getMessageHandler` by name, or as an instance. In any case, the 
-    // expectation is that there will be a MessageHandler of some kind to 
-    // receive `postMessage`.
-    let handler = this.config.handler
-    if (handler) {
-      if (typeof handler === 'string') {
-        if (handler === 'swift') {
-          this.messageHandler = MessageHandler.swift
-        } else {
-          this.messageHandler = getMessageHandler(handler) ?? new MessageHandler(this)
-        }
-      } else {
-        this.messageHandler = handler
-      }
-    } else {
-      this.messageHandler = new MessageHandler(this)
-    }
-
-    // Assign a generated `muId` to the document or shadow root. We can get 
-    // `muId` from the `view.root.muId` if we have `view`, or directly from the `editor`.
-    // Note `muId` is distinct from the `id` of the editor div or the web component id 
-    // if that is set.
-    this.muId = this.generateMuId()
-    this.view.root.muId = this.muId
-
-    // Track the Searcher instance for this editor, using the same muId
-    this.searcher = new Searcher()
-
-    // Track the ID of the selected contentEditable element (relevant when 
-    // there is more than one; otherwise is `editor` or null)
-    this.selectedID = null
-
-    // Finally, track the editor in the muRegistry.
-    registerEditor(this)
+    // Append the container to the shadow DOM
+    // The rest of the initialization happens in the `connectedCallback`
+    shadow.appendChild(this.editorContainer)
   }
-
-  createSelectionBetween(view, $anchor, $head) {
-    const divType = view.state.schema.nodes.div;
-    const range = $anchor.blockRange($head);
-    // Find the divs that the anchor and head reside in.
-    // Both, one, or none can be null.
-    const fromDiv = outermostOfTypeAt(divType, range.$from);
-    const toDiv = outermostOfTypeAt(divType, range.$to);
-    // If selection is all within one div, then default occurs; else return existing selection
-    if ((fromDiv || toDiv) && !$anchor.sameParent($head)) {
-      if (fromDiv != toDiv) {
-        return view.state.selection;    // Return the existing selection
-      }
-    };
-    resetSelectedID(fromDiv?.attrs.id ?? toDiv?.attrs.id ?? null)  // Set the selectedID to the div's id or null.
-    selectionChanged(this.element);
-    // clicked(); // TODO: Removed, but is it needed in Swift MarkupEditor?
-    return null;                        // Default behavior should occur
-  }
-
-  /* Return a string ID we can use for this MarkupEditor */
-  generateMuId() {
-    const timestamp = Date.now().toString(36); // Convert timestamp to base36
-    const randomPart = Math.random().toString(36).substring(2, 7); // Add a short random string
-    return timestamp + randomPart;
-}
 
   /**
-   * Destroy the EditorView we are holding onto and remove it from the `muRegistry`.
-   */
-  destroy() {
-    unregisterEditor(this)
-    this.view.destroy()
+   * Fires after the MarkupEditorElement instance has been attached to the DOM.
+   *  
+   * If no MarkupEditorElement has yet been connected, we invoke `appendEditorScript`
+   * to cause the MarkupEditor script, `markupeditor.umd.js`, to load at the end 
+   * of `body`. This means the MarkupEditor script is loaded only once. When the 
+   * MarkupEditorScript has loaded, it dispatches a `muCallback` event on each 
+   * MarkupEditorElement in the `document`, which in turn creates a properly 
+   * configured MarkupEditor instance.
+   */ 
+  connectedCallback() {
+    this.loadUserFiles()
   }
+
+  /**
+   * Fires when the MarkupEditorElement instance is removed from the DOM.
+   * 
+   * In the spirit of undoing what `connectedCallback` did, we have to destroy
+   * the ProseMirror EditorView held by the MarkupEditor instance in `this.editor`
+   * as well as remove it from the `window.viewRegistry`. The editor does this in 
+   * its `destroy` method.
+   */
+  disconnectedCallback() {
+    this.editor.destroy();
+	}
+
+  /**
+   * Load the CSS from `userstyle` and the script from `userscript`.
+   * 
+   * For the CSS, we append the CSSStyleSheet containing the fetched contents of the 
+   * file specified in `userstyle` to the adoptedStyleSheets that along with
+   * the three required styles already defined via import. Then we use the "standard" 
+   * `MU.loadUserFiles` only specifying the `userScript`.
+   **/ 
+  loadUserFiles() {
+    const nonce = this.getAttribute('nonce')
+    const userStyle = this.getAttribute('userstyle')
+    if (userStyle) {
+      fetch(userStyle)
+        .then((response) => response.text())
+        .then((text) => {
+          // A fetch failure returns 'Cannot GET <filename with path>'
+          let userSheet = new CSSStyleSheet()
+          // replace all styles synchronously:
+          userSheet.replaceSync(text)
+          this.shadowRoot.adoptedStyleSheets = [mirrorStyle, markupStyle, toolbarStyle, userSheet]
+        })
+        .then(() => {
+          const userScript = this.getAttribute('userscript')
+          MU.loadUserFiles(userScript, null, this.editorContainer, nonce)
+        })
+        .catch((e) => {
+          console.log(e)
+        })
+    } else {
+      this.shadowRoot.adoptedStyleSheets = [mirrorStyle, markupStyle, toolbarStyle]
+      const userScript = this.getAttribute('userscript')
+      MU.loadUserFiles(userScript, null, this.editorContainer, nonce)
+    }
+  }
+
+  /**
+   * Create the MarkupEditor instance for this MarkupEditorElement.
+   * 
+   * Use the attributes from the <markup-editor> element to set up the 
+   * configuration. Set the initial HTML based on the `innerHTML` for the 
+   * <markup-editor> element, which will be overridden by `filename` contents 
+   * if it it specified and if the editor is running in an environment that 
+   * has access to the file system (e.g., node.js, but not a browser).
+   */
+  createEditor() {
+    const html = (this.innerHTML.length == 0) ? null : this.innerHTML
+    const filename = this.getAttribute('filename')
+    const config = { 
+      filename: filename, 
+      html: html,
+      base: this.getAttribute('base'),
+      placeholder: this.getAttribute('placeholder'), 
+      delegate: this.getAttribute('delegate'),
+      handler: this.getAttribute('handler'),
+      toolbar: this.getAttribute('toolbar'),
+      behavior: this.getAttribute('behavior'),
+      keymap: this.getAttribute('keymap'),
+      prepend: this.getAttribute('prepend'),
+      append: this.getAttribute('append'),
+    }
+
+    // Create an editor instance and hold onto it here
+    this.editor = new MU.MarkupEditor(this.editorContainer, config)
+    
+    // Let the delegate know the editor is ready
+    //this.editor.config.delegate?.markupReady && this.editor.config.delegate?.markupReady()
+
+    // Prepend and/or append any augmentations
+    const prependItems = MU.getAugmentation(config.prepend)?.menuItems
+    if (prependItems) MU.prependToolbar(prependItems)
+    const appendItems = MU.getAugmentation(config.append)?.menuItems
+    if (appendItems) MU.appendToolbar(appendItems)
+
+    // Expose the MU API as a property of this element. In a well-behaved JavaScript 
+    // modules world, we would not need to do this, but when using the web component 
+    // in a Swift or Electron app, where we use IPC to invoke functionality via 
+    // `executeJavaScript`, MU isn't properly importable. In this case, in a script, 
+    // if we can locate the web component, then we can use its `MU` property to 
+    // invoke MarkupEditor functionality. This is how a "native" menu in Electron 
+    // and how a "native" SwiftUI toolbar in Swift invoke editing functionality.
+    this.MU = MU
+  }
+
 }
 
-/**
- * The public MarkupEditor API callable as "MU.<function name>"
- */
-export const MU = {
-  setTopLevelAttributes,
-  setMessageHandler,
-  loadUserFiles,
-  searchFor,
-  deactivateSearch,
-  cancelSearch,
-  pasteText,
-  pasteHTML,
-  emptyDocument,
-  getHTML,
-  getTestHTML,
-  setHTML,
-  setTestHTML,
-  getHeight,
-  padBottom,
-  focus,
-  focusOn,
-  resetSelection,
-  addDiv,
-  removeDiv,
-  addButton,
-  removeButton,
-  removeAllDivs,
-  toggleBold,
-  toggleItalic,
-  toggleUnderline,
-  toggleCode,
-  toggleStrike,
-  toggleSubscript,
-  toggleSuperscript,
-  setStyle,
-  replaceStyle,
-  toggleListItem,
-  indent,
-  outdent,
-  startModalInput,
-  endModalInput,
-  getSelectionState,
-  consoleLog,
-  doUndo,
-  doRedo,
-  testBlockquoteEnter,
-  testListEnter,
-  testExtractContents,
-  testPasteHTMLPreprocessing,
-  testPasteTextPreprocessing,
-  openLinkDialog,
-  insertLink,
-  deleteLink,
-  getDataImages,
-  savedDataImage,
-  openImageDialog,
-  insertImage,
-  modifyImage,
-  cutImage,
-  insertTable,
-  addRow,
-  addCol,
-  addHeader,
-  deleteTableArea,
-  borderTable,
-  // Allow access to the MarkupEditor class and the instance config
-  MarkupEditor,
-  // Helpers to create custom toolbar items
-  MenuItem, 
-  Dropdown, 
-  DropdownSubmenu, 
-  cmdItem, 
-  renderGrouped, 
-  renderDropdownItems,
-  toolbarView,
-  toggleSearch,
-  // Helpers to add items to the toolbar
-  prependToolbar, 
-  appendToolbar,
-  // Config access
-  ToolbarConfig,
-  KeymapConfig,
-  BehaviorConfig,
-  // muRegistry access
-  activeEditor,
-  registerDelegate,
-  registerConfig,
-  registerMessageHandler,
-  getMessageHandler,
-  activeConfig,
-  registerAugmentation,
-  getAugmentation,
-  setActiveView,
-}
+// Let the browser know about the custom element
+customElements.define('markup-editor', MarkupEditorElement)
