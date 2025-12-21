0032025b52759a575ffa100156c12a5041197976
Steven G. Harris <steven.g.harris@gmail.com>
2025-12-21 10:26:47 -0800

    Use rollup-plugin-import-css

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,13 +1,28 @@
 import resolve from '@rollup/plugin-node-resolve';
+import css from 'rollup-plugin-import-css';
 
 export default [
+	// browser-friendly UMD build, used in Swift WKWebView :-(
+	//{
+	//	input: ['src/main.js'],
+	//	output: {
+	//		file: "dist/markup-editor.umd.js",
+	//		format: 'umd',
+    //        name: 'MU',  // so we can call MU.<exported function> from Swift
+	//	},
+	//	plugins: [
+	//		resolve(), 	// so Rollup can find `ms`
+	//		css()		// so we can import css
+	//	]
+	//},
 	{
 		input: 'src/main.js',
 		output: [
-			{ file: "markupeditor.esm.js", format: 'es' }
+			{ file: "dist/markup-editor.js", format: 'es' }
 		],
 		plugins: [
 			resolve(),
+			css()		// so we can import css
 		]
 	}
 ];
